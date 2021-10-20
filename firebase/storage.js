c428bb71ea0ff904cf52ca7c31f195abe6d68599
Ahmed21kh <92892926+Ahmed21kh@users.noreply.github.com>
2021-10-20 23:57:21 +0200

    Delete firebase directory

--- firebase/storage.js
+++
@@ -1,7 +0,0 @@
-import { storage } from "./config";
-import { PRODUCTS } from "./const/storage";
-
-export const uploadToStorage =(file) =>{
-const fileRef = storage.ref(`${PRODUCTS}/${file.name}`);
-return fileRef.put(file).then(res => res.ref.getDownloadURL());
-}
