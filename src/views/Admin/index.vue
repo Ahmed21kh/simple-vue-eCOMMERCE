6d6cb4159e7ad3f935c2ac8ea402f3709cbe1dab
Ahmed21kh <92892926+Ahmed21kh@users.noreply.github.com>
2021-10-20 23:52:52 +0200

    Create index.vue

---
+++ src/views/Admin/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="admin-content-body" :class="{'nav-opened':navVisibility}">
+  
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@1,100&display=swap" rel="stylesheet"/>
+    <h1></h1>
+    <admin-header></admin-header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import AdminHeader from "@/components/Admin/Header";
+
+export default {
+  components: {
+    AdminHeader,
+  },
+  computed:{
+    navVisibility(){
+      return this.$store.state.admin.navVisibility;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#admin-content-body
+{
+  padding: 0px;
+}
+
+</style>
