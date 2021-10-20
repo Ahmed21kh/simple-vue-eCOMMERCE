5e940446b010cc4b725e2778885c1dcc6ec5c93d
Ahmed21kh <92892926+Ahmed21kh@users.noreply.github.com>
2021-10-20 23:49:10 +0200

    Add files via upload

---
+++ src/components/shared/VELoader.vue
@@ -0,0 +1,74 @@
+<template>
+  <div :class="[`size-${size}`, 'lds-ring']"><div></div><div></div><div></div><div></div></div>
+</template>
+
+<script>
+export default {
+    name:'ve-loader',
+     props: {
+    size: {
+      type: String,
+      required: false,
+      default: "default",
+      validator: function(value) {
+        // The value must match one of these strings
+        return ["default", "small", "large"].indexOf(value) !== -1;
+      }
+    },
+    color: {
+      type: String,
+      required: false,
+      default: "default",
+      validator: function(value) {
+        // The value must match one of these strings
+        return ["default", "white"].indexOf(value) !== -1;
+      }
+    },
+
+}
+}
+</script>
+
+<style lang="scss" scoped>
+.lds-ring {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+
+}
+.lds-ring {
+  div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 34px;
+  height: 34px;
+  margin-left: 20px;
+  margin-top: -20px;
+  border: 4px solid rgb(10, 109, 10);
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: rgb(89, 212, 144) transparent transparent transparent;
+ }
+ 
+
+}
+.lds-ring div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+.lds-ring div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+.lds-ring div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+@keyframes lds-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+</style>
---
+++ src/components/shared/VEModal.vue
@@ -0,0 +1,66 @@
+<template>
+<transition name="fade">
+  <div class="modal-container" v-if="show" @click.self="$emit('close')">
+      <div class="modal-content" :style="{'max-width':width+'px'}">
+       <slot></slot>
+
+      </div>
+  </div>
+</transition>
+</template>
+
+<script>
+export default {
+    name:'ve-modal',
+    props:{
+        show:{
+            type:Boolean,
+            required:false,
+            default:false
+
+        },
+        width:{
+            type:Number,
+            required:false,
+            default:500
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.modal-container
+{
+    position: fixed;
+    top:0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: 50;
+    background-color: rgba(0,0,0,0.5);
+    padding: 20px;
+    box-sizing: border-box;
+    &.fade-enter,
+    &.fade-leave-to{
+        opacity: 0;
+    }
+    &.fade-enter-active,
+    &.fade-leave-active{
+        transition: opacity 0.3s;
+    }
+
+    
+    .modal-content
+    {
+        margin: 50px auto;
+        width: 100%;
+        background-color: white;
+        padding: 20px;
+        border-radius: 5px;
+      
+        box-sizing: border-box;
+        
+    }
+}
+</style>
